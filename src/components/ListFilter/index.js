4f9e66010939376274cbfe012d3dc4f38a90e7dd
catli-unbounce <catherine.li@unbounce.com>
2021-10-16 10:19:03 -0700

    Setting up css components

---
+++ src/components/ListFilter/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import styles from './ListFilter.module.scss';
+const ListFilter = () => {
+    return (
+        <div className={styles["filters"]}>
+            Filters
+        </div>
+    )
+}
+
+export default ListFilter;
