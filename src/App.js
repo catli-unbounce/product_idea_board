ea4de9d105d8f238b365f76902d9bf99e865fbef
catli-unbounce <catherine.li@unbounce.com>
2021-10-18 07:58:43 -0700

    Fetched data from local json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import ListFilter from './components/ListFilter';
 import Roadmap from './components/Roadmap';
 import Header from './components/Header';
 import Suggestions from './components/Suggestions';
+
 function App() {
+
+  const [data, setData] = useState({
+    'productRequests': [],
+    'currentUser': {}
+  })
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/data.json');
+      const fetchedData = await response.json(response);
+      setData({
+        productRequests: fetchedData.productRequests,
+        currentUser: fetchedData.currentUser
+      });
+      console.log(data)
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <main>
