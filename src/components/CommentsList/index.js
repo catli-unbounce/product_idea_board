604cd4392344dbb40ba321f325ef0207483ec1f2
catli-unbounce <catherine.li@unbounce.com>
2021-10-27 19:14:20 -0700

    Comments section CSS

--- src/components/CommentsList/index.js
+++ src/components/CommentsList/index.js
@@ -4,10 +4,26 @@ import {
     useParams
   } from "react-router-dom";
 const CommentsList = ({comments}) => {
-
-    const commentsList = comments.map((item) => {
+    console.log(process.env.PUBLIC_URL)
+    const commentsList = comments.map((comment) => {
         return (
-            <div>{item.content}</div>
+            <div key={comment.id} className="comments_list__item">
+                <img className="comments_list__user-photo" src={'../../' + comment.user.image}></img>
+                <div className="comments_list__container">
+                    <div className="comments_list__user-details">
+                        <div>
+                            <p className="comments_list__user-fullname">{comment.user.name}</p>
+                            <p>@{comment.user.username}</p>
+                        </div>
+                            
+                        <p>Reply</p>
+                       
+                    </div>
+                    
+                    <div className="comments_list__content">{comment.content}</div>
+                </div>
+                
+            </div>
         )
     })
     return (
