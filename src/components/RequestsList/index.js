e56d1a01ec1d8cfbc33104028e8fd0c06c8745c6
catli-unbounce <catherine.li@unbounce.com>
2021-11-06 19:00:22 -0700

    Add empty list image

--- src/components/RequestsList/index.js
+++ src/components/RequestsList/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RequestsListItem from '../RequestsListItem';
 import emptyImg from '../../assets/suggestions/illustration-empty.svg';
+import {Link} from "react-router-dom";
 const RequestsList = ({productRequests, upvote}) => {
     
     const productRequestsList = productRequests.map((item, index) => {
@@ -14,6 +15,11 @@ const RequestsList = ({productRequests, upvote}) => {
         : 
         <div className="requestsList--empty">
             <img src={emptyImg} alt="No product suggestions"></img>
+            <h3>There is no feedback yet.</h3>
+            <p>Got a suggestion? Found a bug that needs to be squashed?</p>  
+            <p>  We love hearing about new ideas to improve our app.</p>
+            <Link className="banner__add-feedback-link" to="/new"><button className="banner__add-btn">+ Add Feedback</button></Link>
+            
         </div>
     )
 }
